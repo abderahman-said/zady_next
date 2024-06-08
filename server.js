8bf0adddd80a5a2edce42e32cd1b9cf33ccf288d
abderahman-said <abdorady6500@gmail.com>
2024-06-08 21:03:41 +0300

    d

--- server.js
+++ server.js
@@ -1,21 +1,37 @@
-const next = require('next') 
-const http = require('http') 
-  const hostName = "egy-home.net"
-  const port = 3016
-const app = next({dev: process.env.NODE_ENV !== 'production'} , hostName , port) 
-  
-app.prepare().then(() => { 
- const server = http.createServer((req, res) => { 
-   // Handle API routes 
-   if (req.url.startsWith('/api')) { 
-     // Your API handling logic here 
-   } else { 
-     // Handle Next.js routes 
-     return app.getRequestHandler()(req, res) 
-   } 
- }) 
- server.listen(port, (err) => { 
-   if (err) throw err 
-   console.log(`> Ready on http://${hostName}:${port}`) 
- }) 
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+ 
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'egy-home.net'
+const port = 3021
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
+ 
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+    try {
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+ 
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query)
+      } else {
+        await handle(req, res, parsedUrl)
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  })
+    .once('error', (err) => {
+      console.error(err)
+      process.exit(1)
+    })
+    .listen(port, () => {
+      console.log(`> Ready on http://${hostname}:${port}`)
+    })
 })
